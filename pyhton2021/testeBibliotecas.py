639e0b48c9644558f4a1b4e260d8f6883a0803a1
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2021-09-17 00:36:29 -0300

    Novos arquivos Python

---
+++ pyhton2021/testeBibliotecas.py
@@ -0,0 +1,17 @@
+# Bibliotecas padrão do Python:
+# Documentação no site oficial: https://docs.python.org/pt-br/3.8/library/index.html
+import random
+sequencia = 1,2,3,4,5,6,7,8,9
+print(random.choice(sequencia))
+
+
+# Bibliotecas criadas por colegas da comunidade:
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))
+
+# Link visualizar emojis: https://carpedm20.github.io/emoji/
+# Link Biblioteca no site do Python: https://pypi.org/project/emoji/
+
+# adicionar/alterar bibliotecas em Python:
+# File -> settings -> project<nomeDoprojeto> -> python Interpreter -> escolher a biblioteca que quer e adicionar
+
